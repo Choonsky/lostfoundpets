9ae6a79a66a747c55b3012ede4d63770f77af056
Stanislav Nemirovsky <choonsky@gmail.com>
2023-01-04 09:05:11 +0600

    Added MongoDB init

--- src/main/java/com/nemirovsky/lostfoundpets/LostFoundPetsApplication.java
+++ src/main/java/com/nemirovsky/lostfoundpets/LostFoundPetsApplication.java
@@ -1,13 +1,29 @@
 package com.nemirovsky.lostfoundpets;
 
+import com.nemirovsky.lostfoundpets.model.Pet;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 
 @SpringBootApplication
-public class LostFoundPetsApplication {
+@RequiredArgsConstructor
+public class LostFoundPetsApplication implements CommandLineRunner {
+
+    private ReactiveMongoTemplate mongoTemplate;
 
     public static void main(String[] args) {
         SpringApplication.run(LostFoundPetsApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        setup();
+    }
+
+    private void setup() {
+        Pet p1 = new Pet();
+        mongoTemplate.save(p1);
+    }
 }
