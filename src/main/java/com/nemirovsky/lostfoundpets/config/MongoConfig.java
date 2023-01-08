b2b734739aab661fd5d9f72c5f3de074f58c67d8
Stanislav Nemirovsky <choonsky@gmail.com>
2023-01-08 19:56:57 +0600

    Fixed AWS DocumentDB connection config #4

--- src/main/java/com/nemirovsky/lostfoundpets/config/MongoConfig.java
+++ src/main/java/com/nemirovsky/lostfoundpets/config/MongoConfig.java
@@ -6,15 +6,14 @@ import com.mongodb.reactivestreams.client.MongoClient;
 import com.mongodb.reactivestreams.client.MongoClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 
 @Configuration
 public class MongoConfig {
     @Bean
     public MongoClient mongo() {
-        ConnectionString connectionString = new ConnectionString("mongodb://DocumentDBAdmin:KrolikEst_1@docdb-2023-01-08-13-20-47.cluster-cuk3aks6r1zn.ap-northeast-1.docdb\n" +
-                "      .amazonaws.com:27017/lostfoundpets?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false");
+        ConnectionString connectionString = new ConnectionString("mongodb://DocumentDBAdmin:KrolikEst_1@docdb-2023-01" +
+                "-08-13-20-47.cluster-cuk3aks6r1zn.ap-northeast-1.docdb.amazonaws.com:27017/?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false");
         MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
                 .applyConnectionString(connectionString)
                 .build();
